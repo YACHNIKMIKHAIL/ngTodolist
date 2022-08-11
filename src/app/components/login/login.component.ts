df53c01b1d660510747b5598d48a2b317b36eb28
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-08-11 14:02:14 +0200

    login validation is realized commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 import {LoginService} from "../../services/login.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -21,10 +22,35 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  form = new FormGroup({
+    email: new FormControl<string>('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(5)
+    ]),
+    pass: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(5)
+    ])
+  })
+
+  get eMail() {
+    return this.form.controls.email as FormControl
+  }
+
+  get password() {
+    return this.form.controls.pass as FormControl
+  }
+
+  submit() {
+    if (this.form.controls.email.errors || this.form.controls.pass.errors) return
+    this.logIn()
+  }
+
   logIn() {
     this.loginService.logIn({
-      email: this.email,
-      password: this.pass,
+      email: this.form.value.email as string,
+      password: this.form.value.pass as string,
       rememberMe: true
     }).subscribe(res => {
       !!res.data.userId && this.loginService.isAuthFunc(true, res.data.userId.toString())
